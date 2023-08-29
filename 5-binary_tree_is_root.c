c637c418f9c691936e8f2925683b1b2600f56da3
dominic-source <chinonsodomnic@gmail.com>
2023-08-29 15:19:37 -0700

    detect root node

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check if a tree is the root
+ * @node: the node of the tree
+ * Return: 1 or 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node->parent != NULL)
+		return (0);
+	return (1);
+}
