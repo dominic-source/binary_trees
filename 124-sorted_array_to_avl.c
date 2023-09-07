238d8ce85a8ac4deffc35245a055ac44fdd99936
dominic-source <chinonsodomnic@gmail.com>
2023-09-07 01:43:01 -0700

    debug sorted array insertion

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -7,20 +7,22 @@
  * @array: array of integers
  * Return: Node  or NULL
  */
-avl_t *recursion(avl_t *parent, size_t start, size_t end, int *array)
+binary_tree_t *recursion(binary_tree_t *parent, size_t start,
+			 size_t end, int *array)
 {
-	avl_t *node;
+	binary_tree_t *node;
 	size_t mid;
+	int value;
 
-	if (start >= end)
+	if (start > end || end < 1)
 		return (NULL);
 
-	mid = (end + start) / 2;
-	node = (avl_t *)binary_tree_node((binary_tree_t *)parent, array[mid]);
+	mid = (start + end) / 2;
+	value = array[mid];
+	node = binary_tree_node(parent, value);
 
 	if (node == NULL)
 		return (NULL);
-
 	node->left = recursion(node, start, mid - 1, array);
 	node->right = recursion(node, mid + 1, end, array);
 
@@ -34,7 +36,10 @@ avl_t *recursion(avl_t *parent, size_t start, size_t end, int *array)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
+	binary_tree_t *node;
+
 	if (array == NULL || size == 0)
 		return (NULL);
-	return (recursion(NULL, 0, size - 1, array));
+	node = recursion(NULL, 0, size - 1, array);
+	return ((avl_t *)node);
 }
